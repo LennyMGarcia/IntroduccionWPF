e6de37afd48880f63857599041032be93914aafc
Lenny <Lennymgr27@gmail.com>
2022-06-04 15:05:04 -0400

    Agregar archivos de proyecto.

---
+++ IntroduccionWPF/JuntaNombre.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduccionWPF
+{
+    //INotifyPropertyChanged sirve para que cuando un objeto modifique una propiedad cualquiera este desencadenen una accion
+    //Reune todo los enemtos que desencadena un objeto en uno solo
+    //se llama a la interfaz INotiy...
+    class JuntaNombre:INotifyPropertyChanged
+    {
+
+        //Se indica los nombres
+        private string nombre, apellido, nombre_Completo;
+        //El eventHandler es el que controlara property changed, este sera la guia para el eventArgs
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string property)
+        {//este tiene lugar cuando se cambia una propiedad
+            //cuando sera difererente de null
+            if (PropertyChanged != null)
+            {//dira que evento se desencadena en el string de property, a traves de un valor string
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public string Nombre
+        {
+            get{ return nombre; }
+            set{ nombre = value;
+
+                OnPropertyChanged("Nombre_Completo"); }
+        }
+
+        public string Apellido
+        {
+            get{ return apellido; }
+            set
+            {
+                apellido = value;
+                //Hace que el valor se reprensente en el campo nombrado en este caso es Nombre_Completo
+                OnPropertyChanged("Nombre_Completo");
+            }
+        }
+
+        public string Nombre_Completo
+        {
+            get{  nombre_Completo = nombre + " " + apellido;
+                return nombre_Completo;
+            }
+            set {  }
+
+        }
+
+
+    }
+}
---
+++ IntroduccionWPF/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntroduccionWPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            //Aparece estos nombres como base
+            JuntaNombreYApellido = new JuntaNombre { Nombre = "Juan", Apellido = "Diaz" };
+            //Indica el origen de los datos indicando los del nombre y apellido
+            this.DataContext = JuntaNombreYApellido;
+        }
+
+        private JuntaNombre JuntaNombreYApellido;
+
+    }
+
+}
+
